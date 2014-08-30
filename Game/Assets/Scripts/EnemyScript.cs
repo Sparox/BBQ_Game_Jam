40b4c6e15f5eb6dbe42fbaa72625146da9186e87
Cédric CORNELIS <cedric.cornelis@supinfo.com>
2014-08-30 17:34:57 +0200

    scens

--- Game/Assets/Scripts/EnemyScript.cs
+++ Game/Assets/Scripts/EnemyScript.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class EnemyScript : MonoBehaviour {
 
+	public float enemyValue = 1f;
+	HUDScript hud;
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		//If collide with Player
 		if (other.tag == "Player") 
 		{
-			//Lvl up
+			//On augmente le score en fonction de la valeur de l'enemy
+			hud = GameObject.Find("Main Camera").GetComponent<HUDScript>();
+			hud.IncreaseScore(enemyValue);
+
+			//On kill l'enemy
 			Destroy(this.gameObject);
 		}
 	}
---
+++ Game/Assets/Scripts/HUDScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDScript : MonoBehaviour {
+
+	float playerScore = 0;
+
+	void Update() {
+		//playerScore += Time.deltaTime;
+	}
+	
+	public void IncreaseScore(float amount)
+	{
+		playerScore += amount;
+	}
+	
+	public void DecreaseScore(int amount)
+	{
+		playerScore -= amount;
+	}
+	void OnDisable()
+	{
+		PlayerPrefs.SetInt("Score", (int)playerScore * 100);
+	}
+	void OnGUI()
+	{
+		GUI.Label(new Rect(10,10,100,30), "Score : " + (int)(playerScore * 100));
+	}
+}
