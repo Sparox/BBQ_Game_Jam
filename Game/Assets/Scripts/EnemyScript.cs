73c579b3405bdf1e29ae0b6966e8cf93b66df840
Cédric CORNELIS <cedric.cornelis@supinfo.com>
2014-08-30 15:40:34 +0200

    enemyscript

---
+++ Game/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		//If collide with Player
+		if (other.tag == "Player") 
+		{
+			//Lvl up
+			Destroy(this.gameObject);
+		}
+	}
+}
