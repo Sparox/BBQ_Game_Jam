6048654102c326a26c44ef1d05e7ce00a0266b91
Cédric CORNELIS <cedric.cornelis@supinfo.com>
2014-08-31 09:46:17 +0200

    tuni

--- Game/Assets/Scripts/MenuScript.cs
+++ Game/Assets/Scripts/MenuScript.cs
@@ -13,9 +13,19 @@ public class MenuScript : MonoBehaviour {
 
 	void OnGUI () {
 		//GUI.Label (new Rect (Screen.width / 2 - 40, 50, 80, 30), "Game Over");
+		string scoreString = "<color=red><size=100>Score : " + score + "</size></color>";
+		int scoreWidth = scoreString.Length * 10;
 
-		GUI.Label (new Rect (Screen.width/2, 50, 1500, 1500), "<color=red><size=100>Score : " + score + "</size></color>");
-		if (GUI.Button (new Rect (Screen.width / 2, Screen.height/2, 300, 100), "<color=red><size=20>Play again !</size></color>")) 
+		string retryString = "<color=red><size=20>Play again !</size></color>";
+		int retryWidth = retryString.Length * 10;
+		//10 is a constant which will obviously vary as you change the font or its size,
+		//so you'll need a little of pacience to get a good value there
+		
+		//GUI.Label(new Rect(Screen.width/2 - textWidth/2, Screen.height/2, textWidth, 20), myString);
+
+
+		GUI.Label (new Rect (Screen.width / 2 - scoreWidth / 2, 50, scoreWidth+150, 150), scoreString);
+		if (GUI.Button (new Rect (Screen.width / 2 - retryWidth / 2, Screen.height/2, retryWidth, 100), retryString)) 
 		{
 			Application.LoadLevel (0);
 		}
