86fdb3e6935b877ce3781e58a575965ee1005af8
jo541 <si2.touba@chopin.net>
2014-08-31 08:37:34 +0200

    ajout des sons

---
+++ Game/Assets/Scripts/Bo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		(GameObject.Find ("Bo")).audio.mute = false;
+		(GameObject.Find ("Bo")).audio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
